6b8b03e2d5f47cb76e17a11cb80da1f0f135d5c4
Eduard <edusellasinsti@gmail.com>
2021-10-14 18:09:02 +0200

    registre

--- validacioregistre.php
+++ validacioregistre.php
@@ -2,10 +2,20 @@
 
 include 'database.php';
 
-if(isset($_GET['nom'],$_GET['cognom'],$_GET['usuari'],$_GET['correu'],$_GET['tel'],$_GET['password'])){
+if(!isset($_GET['nom'],$_GET['dni'],$_GET['cognom'],$_GET['usuari'],$_GET['mail'],$_GET['telefon'],$_GET['contrasenya'])){
     echo('Location:singup.php');
     //header('Location:singup.php');
 }
-echo('hola');
+//echo('hola');
+$nom= $_GET['nom'];
+$dni=$_GET['dni'];
+$cognom =$_GET['cognom'];
+$usuari=$_GET['usuari'];
+$mail=$_GET['mail'];
+$telefon=$_GET['telefon'];
+$contrasenya=$_GET['contrasenya'];
 
-?>
+$sql = "INSERT INTO `usuari` (`DNI`, `Nom`, `Cognom`, `tel`, `correu`, `rol`, `username`, `password`, `id_departament_usuari`) VALUES ('".$dni."', '".$nom."', '".$cognom."', '".$telefon."', '".$mail."', '0', '".$usuari."', '".$contrasenya."', '0');";
+//http://localhost/hotelcendrassos/validacioregistre.php?mail=eduardsellaslleo%40gmail.com&nom=Eduard&cognom=Sellas+Lleo&dni=41563450s&telefon=616161166&usuari=edu&contrasenya=Hola1234
+
+echo($sql);

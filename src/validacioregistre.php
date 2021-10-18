346b57c0073b0287715deac372a221c567f8170a
eimantas <milkintaseimantas@gmail.com>
2021-10-18 19:49:36 +0200

    18/10/2021

--- src/validacioregistre.php
+++ src/validacioregistre.php
@@ -16,13 +16,17 @@ $telefon=$_GET['telefon'];
 $contrasenya=$_GET['contrasenya'];
 
 $stm = $conn->prepare("INSERT INTO `usuari` (`DNI`, `Nom`, `Cognom`, `tel`, `correu`, `rol`, `username`, `password`, `id_departament_usuari`) VALUES ( :dni , :nom, :cognom, :tel , :mail , '0', :usuari , :pass , '0');");
-$sql = $stm->execute([':dni' => $dni],[':nom' => $nom],[':cognom' => $cognom],[':usuari' => $usuari],[':mail' => $mail], [':tel' => $telefon],[':pass' => $contrasenya]);
 
-//$sql = "INSERT INTO `usuari` (`DNI`, `Nom`, `Cognom`, `tel`, `correu`, `rol`, `username`, `password`, `id_departament_usuari`) VALUES ('".$dni."', '".$nom."', '".$cognom."', '".$telefon."', '".$mail."', '0', '".$usuari."', '".$contrasenya."', '0');";
-//http://localhost/hotelcendrassos/src/validacioregistre.php?mail=eduardsellaslleo%40gmail.com&nom=Eduard&cognom=Sellas+Lleo&dni=41563450s&telefon=616161616&usuari=edusell&contrasenya=Hola1234
-if ($conn->query($sql) === TRUE) {
-    echo "OK";      
-  }else {
-    echo "ERROR";
-  }
-//echo($sql);
+$sql = $stm->execute([
+  ':dni' => $dni,
+  ':nom' => $nom,
+  ':cognom' => $cognom,
+  ':usuari' => $usuari,
+  ':mail' => $mail,
+  ':tel' => $telefon,
+  ':pass' => $contrasenya,
+]);
+if($stm -> rowCount() > 0){
+  echo 'ja existeix';
+}
+

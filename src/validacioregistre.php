3c207bc132a891af9da9ddd6a2c53e37b49047ff
eimantas <milkintaseimantas@gmail.com>
2021-10-21 17:28:49 +0200

    21/10/2021

--- src/validacioregistre.php
+++ src/validacioregistre.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(0);
+session_start();
 include 'database.php';
 
 if(!isset($_GET['nom'],$_GET['dni'],$_GET['cognom'],$_GET['usuari'],$_GET['mail'],$_GET['telefon'],$_GET['contrasenya'])){
@@ -23,10 +24,43 @@ $sql1 = $stm1->execute([
 ]);
 
 //Verificar si existeix el usuari a registrar
+$caracters_dni=strlen($dni);
+$caracts_numero_tlf=false;
+$errvalid=false;
+$permitidos = "0123456789";
+  for ($i=0; $i<strlen($telefon); $i++){
+    if (strpos($permitidos, substr($nombre_usuario,$i,1))===false){
+      $caracts_numero_tlf = true;
+    }
+ }
+
+
+ if($caracters_dni<9){
+  header("Location: ../public/registre.php?caracters_dni=true");
+  $errvalid=true;
+}
+if($caracts_numero_tlf==true){
+  header("Location: ../public/registre.php?caracters_tlf=true");
+  $errvalid=true;
+}
+if(strlen($telefon)<9){
+  header("Location: ../public/registre.php?largada_tlf=true");
+  $errvalid=true;
+}
+if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
+  header("Location: ../public/registre.php?mail_err=true");
+  $errvalid=true;
+}
+if(!checkdnsrr(array_pop(explode("@",$mail)),"MX")){
+  header("Location: ../public/registre.php?mail_err=true");
+  $errvalid=true;
+}
 if($stm1->rowCount()>0){
-  header("Location: ../public/registre.html");
+  header("Location: ../public/registre.php?dni=true");
+  $errvalid=true;
   
-}else{
+}
+if($errvalid==false){
   $sql = $stm->execute([
     ':dni' => $dni,
     ':nom' => $nom,

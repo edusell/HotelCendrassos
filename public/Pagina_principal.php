e72ed1a83a1f1a37488d43512bc324c4e825b80f
Eduard <edusellasinsti@gmail.com>
2021-10-22 15:13:02 +0200

    22/10/2021

--- public/Pagina_principal.php
+++ public/Pagina_principal.php
@@ -1,80 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="css.css"rel="stylesheet" type="text/css">
-    <title>Document</title>
-    <?php $data_avui = date("Y-m-d");?>
-</head>
-<body>
-<div class="topnav" id="myTopnav">
-  <a href="Pagina_principal.php" class="active">INICI</a>
-  <a href="habitacions.php">HABITACIONS</a>
-  <a href="galeria.php">GALERIA</a>
-  <a href="contacte.html">CONTACTE</a>
-  <a href="login.html" class=logoinici></a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
-
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <link href="css.css"rel="stylesheet" type="text/css">
+      <title>Document</title>
+      <?php $data_avui = date("Y-m-d");?>
+   </head>
+   <body>
+      <div class="topnav" id="myTopnav">
+         <a href="Pagina_principal.php" class="active">INICI</a>
+         <a href="habitacions.php">HABITACIONS</a>
+         <a href="galeria.php">GALERIA</a>
+         <a href="contacte.html">CONTACTE</a>
+         <a href="login.html" class=logoinici></a>
+         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+         <i class="fa fa-bars"></i>
+         </a>
+      </div>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src="logos\slider_habitacions\habitacio1.jpg" class="d-block w-100" alt="...">
+               <img src="logos\slider_habitacions\habitacio1.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-            <img src="logos\slider_menu_principal\imatge2.jpg" class="d-block w-100" alt="...">
+               <img src="logos\slider_menu_principal\imatge2.jpg" class="d-block w-100" alt="...">
             </div>
-        </div>
-        <div class="reserva">
-                <form action="recerca.php" method='GET'>
-                    <label> Data arribada <input type="date" id="arribada" name="arribada_hotel" value="<?php echo $data_avui?>" min="<?php echo $data_avui?>" max="2100-12-31" required></label>
-                    <label> Data sortida  <input type="date" id="sortida" name="sortida_hotel" value="<?php echo $data_avui?>" min="<?php echo $data_avui?>" max="2100-12-31"></label>
-                    <label> Ocupants  <input type="number" id="ocupants" name="ocupants"min="1" max="6" required></label>
-                    <button >Reserva</button>
-                </form>
-              </div>
-        </div>
-    <div class='submenu'>
-        <p>Hola</p>
+         </div>
+         <div class="reserva">
+            <form action="recerca.php" method='GET'>
+               <label> Data arribada <input type="date" id="arribada" name="arribada_hotel" value="<?php echo $data_avui?>" min="<?php echo $data_avui?>" max="2100-12-31" required></label>
+               <label> Data sortida  <input type="date" id="sortida" name="sortida_hotel" value="<?php echo $data_avui?>" min="<?php echo $data_avui?>" max="2100-12-31"></label>
+               <label> Ocupants  <input type="number" id="ocupants" name="ocupants"min="1" max="6" required></label>
+               <button >Reserva</button>
+            </form>
+         </div>
+      </div>
+      <div id=provasticki>
+         <div class='sticky'>
+             <div class='container-submenu'>
+            <a class='submenu' href='#aventatges'><div>SERVEIS</div></a>
+            <a class='submenu' href='#avantatges'><div>CALENDARI</div></a>
+            <a class='submenul' href='#carouselExampleSlidesOnly'><div> <img src="logos/logo-hotel.png" alt="" width='40px'> </div></a>
+            <a class='submenu' href='#nosaltres'><div>SOBRE NOSALTRES</div></a>
+            <a class='submenu' href='#aventatges'><div>CONTACTE</div></a>
+            </div>
+         
 
+         </div>
+         <div id="avantatges" class="avantatges">
+            <!--<h1>SERVEIS</h1>
+            <div class='container100'>
+               <table class='serveis'>
+                  <tr>
+                     <td class='doble'>
+                        PARKING VIGILAT
 
-      <!--
-    <div class="avantatges">
-        <h1>Descobreix les avantatges</h1>
-        <div class="avantatges_logos">
-            <div>
-                <img src="logos\logo-parking.png" alt="..." width="60" height="60">
-                <p>Servei parking vigilat</p>
+                     </td>
+                     <td>
+                        <div class='fonsimg'>
+                           <img src="img/inici/prking.jpg" alt="img">
+                        </div>
+                     </td>
+                  </tr>
+               </table>
+            </div>-->
+            <h1>SERVEIS</h1>
+            <div class="avantatges_logos">
+               <div>
+                  <img src="logos\logo-parking.png" alt="..." width="60" height="60">
+                  <p>Servei parking vigilat</p>
+               </div>
+               <div>
+                  <img src="logos\logo-bicicleta.png" alt="..." width="60" height="60">
+                  <p>Servei parking bicicleta</p>
+               </div>
+               <div>
+                  <img src="logos\logo_piscina.png" alt="..." width="60" height="60">
+                  <p>Servei piscina</p>
+               </div>
             </div>
-            <div>
-                <img src="logos\logo-bicicleta.png" alt="..." width="60" height="60">
-                <p>Servei parking bicicleta</p>
-            </div>
-            <div>
-                <img src="logos\logo_piscina.png" alt="..." width="60" height="60">
-                <p>Servei piscina</p>
-            </div>
-        </div>
-    </div>-->
-    <div id=provasticki style='height: 1000px;'>
-
-    </div>
-    </div>  
-    <div class="footerpr">
-       <!-- <footer>
+         </div>-->
+         <div class='nosaltres'>
+            <p>hola</p>
+         </div>
+      
+      <div id="nosaltres">
+        <p>hola</p>
+      </div>
+      </div>  
+      </div>
+      </div>
+      <div class="footerpr">
+         <!-- <footer>
             <div class="logos_footer">
             <img src="logos\footer\twiter_logo.png" alt="..." width="60" height="60">
             <img src="logos\footer\facebook_logo.png" alt="..." width="60" height="60">
             <img src="logos\footer\youtube_logo.png" alt="..." width="60" height="60">
             <img src="logos\footer\instagram_logo.png" alt="..." width="60" height="60">
-
-
-
+            
+            
+            
             </div>
             <ul>
                 <li ><a href="Pagina_principal.php"><p class="footer_seccions">Pagina principal</p></li>
@@ -83,18 +111,18 @@
                 <li><a href="contacta.php"><p class="footer_seccions">Contacta</p></li>
                 <li><a href="serveis.php"><p class="footer_seccions">galeria</p></li>
             </ul>
-        </footer>-->
-    </div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-</script>
+            </footer>-->
+      </div>
+   </body>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   <script>
+      function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+   </script>
 </html>
--- public/recerca.php
+++ public/recerca.php
@@ -14,7 +14,6 @@ $ocupants=$_GET['ocupants'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="css.css"rel="stylesheet" type="text/css">
-    <link href="recerca.css"rel="stylesheet" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
@@ -32,18 +31,24 @@ $ocupants=$_GET['ocupants'];
 
         <?php
         echo($arribada.','.$sortida);
-        include 'database.php';
+        include '../src/database.php';
 
-        $sql = "select h.id_habitacio , t.m_tipus , t.serveis_tipus , t.ocupants_tipus , t.desc_tipus ,t.id_tipus ,t.nom_tipus, r.data_arribada,r.data_sortida FROM habitacio h , tipushabitacio t, reservahabitacio i, reserva r where t.id_tipus=h.id_tipus_habitacio AND h.id_habitacio=i.id_habitacio AND i.id_reserva=r.id_reserva AND t.ocupants_tipus>'".$ocupants."' AND ('".$arribada."'>=r.data_sortida OR ('".$arribada."'<r.data_arribada AND '".$sortida."'<=data_arribada));";
-        $habitacions = $conn->query($sql);
-        echo($sql);
+        $stm1 = $conn->prepare("select h.id_habitacio , t.m_tipus ,t.preu, t.serveis_tipus , t.ocupants_tipus , t.desc_tipus ,t.id_tipus ,t.nom_tipus, r.data_arribada,r.data_sortida FROM habitacio h , tipushabitacio t, reservahabitacio i, reserva r where t.id_tipus=h.id_tipus_habitacio AND h.id_habitacio=i.id_habitacio AND i.id_reserva=r.id_reserva AND t.ocupants_tipus> :ocupants AND ( :arribada >=r.data_sortida OR ( :arribada <r.data_arribada AND :sortida <=data_arribada));");
+        $stm1->execute([
+             ':arribada' => $arribada,
+             ':ocupants' => $ocupants,
+             ':sortida' => $sortida
+        ]);
 
-        while($row = $habitacions->fetch_assoc()) {//recorrem el select
+
+        //print_r($stm1);
+        
+        while($row = $stm1->fetch(PDO::FETCH_ASSOC)) {//recorrem el select
          
-
-            $sql='select r.data_arrivada , r.data_sortida , h.id_reserva , h.id_habitacio FROM reserva r, reservahabitacio h WHERE r.id_reserva=h.id_reserva AND h.id_habitacio='.$row['id_habitacio'];
-            $reserves = $conn->query($sql);
-
+            $stm1 = $conn->prepare('select r.data_arrivada , r.data_sortida , h.id_reserva , h.id_habitacio FROM reserva r, reservahabitacio h WHERE r.id_reserva=h.id_reserva AND h.id_habitacio= :id ;');
+            $reserves = $stm1->execute([
+                ':id' => $row['id_habitacio']
+            ]);
             
 
             if($row['id_tipus']==1){
@@ -51,18 +56,26 @@ $ocupants=$_GET['ocupants'];
             } else {
                 $img = 'habitacio_doble.jpg';
             }
-
-            print '<div class="habitacio">';
-            print '<img src="img/'.$img.'" alt="Girl in a jacket" height="300px">';
-            print '<div>';
-            print "Numero d'habitacio:".$row['id_habitacio']."<br>";
-            print $row['nom_tipus']." ->".$row['m_tipus']."m<sup>2</sup> <br>";
-            print $row['desc_tipus']."<br>";
-            print "Ocupants: ".$row['ocupants_tipus']."<br>";
-            print "Serveis: ".$row['serveis_tipus']."<br>";
-            print '</div>';
-            print '</div>';
-            print "<br><br><br>";
+            
+            print "<div class='container_tipus_habitacions'>";
+            print '<div class="tipus_habitacions">';
+              print"<h3>".$row['nom_tipus']."</h3>";
+              print'<div class="container_imatges_habitacions">';
+                print'<div class="imatges_habitacions"></div>';
+                    print'<div class="text">';
+                        print"<p>".$row['desc_tipus']."</p>";
+                        print"<div class='especificacions'>";
+                            print "<img src='logos\habitacions_logos\silueta-persona.png'>";
+                            print"<p><b>MAX ".$row['ocupants_tipus']." p</b></p>";
+                            print "<img src='logos\habitacions_logos\information.png'>";
+                            print"<p><b>".$row['m_tipus']."m<sub>2 </sub></b></p>";
+                        print"</div>";
+                        print"<div class='preu'>".$row['preu']."€ </div>";
+                        print"<a href='habitacions.php#arribada_hotel'><div class='boto'>Reserva</div></a>";
+                    print"</div>";
+              print"</div>";
+            print"</div>";
+            print "</div>";
           }
 
         ?>

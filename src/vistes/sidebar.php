e117a8cd23b88a079bada50dfbc330c0a13c24dc
Eduard <edusellasinsti@gmail.com>
2021-10-25 18:04:24 +0200

    25/10/2021

---
+++ src/vistes/sidebar.php
@@ -0,0 +1,7 @@
+<div class='sidebar'>
+        <a href="index.php?r=admin" class='menuesquerra actiu'>&nbsp&nbsp<img src="logos/admin_icon/panell.png" width='25px'><div>&nbsp&nbsp PANELL</div><a>
+        <a href="index.php?r=adminusuari" class='menuesquerra'>&nbsp&nbsp<img src="logos/admin_icon/people.png" width='25px'><div>&nbsp&nbsp USUARIS</div><a>
+        <a href="index.php?r=adminreserva" class='menuesquerra'>&nbsp&nbsp<img src="logos/admin_icon/reserves.png" width='25px'><div>&nbsp&nbsp RESERVES</div><a>
+        <a href="index.php?r=admincalendari" class='menuesquerra'>&nbsp&nbsp<img src="logos/admin_icon/calendari.png" width='25px'><div>&nbsp&nbsp CALENDARI</div><a>
+        <a href="index.php?r=adminhabitacio" class='menuesquerra'>&nbsp&nbsp<img src="logos/admin_icon/room.png" width='25px'><div>&nbsp&nbsp HABITACIONS</div><a>
+      </div>

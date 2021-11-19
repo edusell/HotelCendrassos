46ce6b427ff3e563f8f12626bdfc0847f4a4a3ae
Eduard <edusellasinsti@gmail.com>
2021-11-19 15:10:37 +0100

    comentaris, admin-complet

--- src/controlador/panell_reservas.php
+++ src/controlador/panell_reservas.php
@@ -1,5 +1,12 @@
 <?php
 
+
+/**************************
+Hotel Cendrassos
+Autors: Eimantas Milkintas i Eduard Sellas
+Controlador per dirigir al panell reserves del menu d'administracio, llista les reserves amb tota la seva informacio sobre el tipus d'habitracio i s
+***************************/
+
 function ctrlAdminreserva($peticio, $resposta, $imatges){
     include '../src/config.php';
 
